ef07438df775d48f61b69aa74bf843d5224bb699
Lewis Miller <lewis.miller.github@pm.me>
2022-04-25 14:03:22 -0700

    feat: add package landing page

--- packages/pi-ui/pages/index.tsx
+++ packages/pi-ui/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import { Button } from '@nextui-org/react';
 
 import styles from '../styles/Home.module.css';
@@ -17,7 +18,8 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-          <Button>Click me</Button>
+          <br />
+          <Link href="/packages">Packages</Link>
         </h1>
 
         <p className={styles.description}>
