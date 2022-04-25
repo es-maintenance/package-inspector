d45a2e6a36453ce01356e824a60d6177f4649fa5
Lewis Miller <lewis.miller.github@pm.me>
2022-04-25 12:17:26 -0700

    feat: use next-ui

--- packages/pi-ui/pages/index.tsx
+++ packages/pi-ui/pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { Button } from '@nextui-org/react';
+
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
@@ -15,6 +17,7 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <Button>Click me</Button>
         </h1>
 
         <p className={styles.description}>
