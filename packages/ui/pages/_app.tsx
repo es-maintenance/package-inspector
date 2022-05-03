3f9ab299e30745ee39468e2b97799fe2ba65adbb
Lewis Miller <lewis.miller.github@pm.me>
2022-05-03 11:49:49 -0700

    refactor: remove nextui from app

--- packages/ui/pages/_app.tsx
+++ packages/ui/pages/_app.tsx
@@ -5,7 +5,6 @@ import { EmotionCache } from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
-import { NextUIProvider } from '@nextui-org/react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import * as React from 'react';
@@ -33,9 +32,7 @@ export default function MyApp(props: MyAppProps) {
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
           <CssBaseline />
-          <NextUIProvider>
-            <Component {...pageProps} />
-          </NextUIProvider>
+          <Component {...pageProps} />
         </ThemeProvider>
       </CacheProvider>
     </ApolloProvider>
